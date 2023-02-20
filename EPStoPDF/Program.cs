d9ede7a2f32e255c61547c9c055e2c14b166b3c0
matthoward01 <matthoward30740@gmail.com>
2023-02-20 12:49:36 -0500

    Add project files.

---
+++ EPStoPDF/Program.cs
@@ -0,0 +1,58 @@
+﻿using Ghostscript.NET;
+using Ghostscript.NET.Processor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EPStoPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\test.eps";
+            EPStoPDF(file);
+        }
+        public static void EPStoPDF(string printFile)
+        {
+            GhostscriptVersionInfo gvi = new GhostscriptVersionInfo(new Version(0, 0, 0), System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "References", "gsdll64.dll"), string.Empty, GhostscriptLicense.GPL);
+            using (GhostscriptProcessor processor = new GhostscriptProcessor(gvi))
+            {
+                List<string> switches = new List<string>();
+                //switches.Add("-dPrinted");
+                //switches.Add("-dBATCH");
+                //switches.Add("-dNOPAUSE");
+                switches.Add("-dNOSAFER");
+                /*if (tabloid)
+                {
+                    switches.Add("-g792x1224");
+                }
+                if (fit)
+                {
+                    switches.Add("-dPDFFitPage");
+
+                }*/
+                //switches.Add("-dNumCopies=1");
+                switches.Add("-sDEVICE=pdfwrite");
+                switches.Add("-AutoRotatePages=/None");
+                //switches.Add("-ProduceDSC=false");
+                switches.Add("-dSetPageSize=false");
+                switches.Add("-dEPSCrop=true");
+                switches.Add("-dCompressPages=false");
+                switches.Add("-dGrayImageResolution=300");
+                switches.Add("-dPreserveSeparation=true");
+                //switches.Add("-dEmbedAllFonts=true");
+                //switches.Add("-dOptimize=false");
+                //switches.Add("-dDownsampleColorImages=false");
+                //switches.Add("-dDownsampleGrayImages=false");
+                //switches.Add("-dDownsampleMonoImages=false");
+                //switches.Add(Convert.ToString("-sOutputFile=%printer%") + Settings.Default.Printer);
+                
+                switches.Add("-o");
+                switches.Add(Path.Combine(Path.GetDirectoryName(printFile), Path.GetFileNameWithoutExtension(printFile) + ".pdf"));
+                switches.Add(printFile);
+                processor.StartProcessing(switches.ToArray(), null);
+            }
+        }
+    }
+}
